a35d93eb6308624bb271e124fae436fece74c9cb
Katya Sheps <katya@sheps.ru>
2019-06-26 23:19:01 +0300

    Add onSaveInstanceState and onRestoreInstanceState Made null checking in onCreate

--- app/src/main/java/ru/sheps/android/homework611/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework611/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    private static final String SAVED_KEY = "text_for_save";
+    TextView textView;
+    private String savedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Lifecycle", "onCreate");
-        TextView textView = findViewById(R.id.textView);
-        textView.append("\n" + "ОnClick");
-
+        textView = findViewById(R.id.textView);
+        if (savedInstanceState == null) {
+            textView.setText("\n" + "ОnClick");
+        }
+//        else {
+//           // final String savedText = savedInstanceState.getString(SAVED_KEY);
+//            textView.append(savedText + "\n" + "ОnClick");
+//        }
     }
 
     
@@ -27,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: " + this);
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.append("\n" + "ОnStart");
     }
 
@@ -35,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: " + this);
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.append("\n" + "ОnResume");
     }
 
@@ -43,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: " + this);
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.append("\n" + "ОnPaused");
     }
 
@@ -51,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop: " + this);
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.append("\n" + "ОnStop");
     }
 
@@ -59,8 +67,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: " + this);
-        TextView textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.append("\n" + "ОnDestroy");
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyDown:" +this);
+        TextView textView = findViewById(R.id.textView);
+        textView.append("\n" + "onKeyDown");
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyLongPress: " + this);
+        textView = findViewById(R.id.textView);
+        textView.append("\n" + "onKeyLongPress");
+        return super.onKeyLongPress(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "onBackPressed: " + this);
+        textView = findViewById(R.id.textView);
+        textView.append("\n" + "onBackPressed");
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: " + this);
+        textView = findViewById(R.id.textView);
+        textView.append("\n" + "onSaveInstanceState");
+        String textForSave = textView.getText().toString();
+        outState.putString(SAVED_KEY, textForSave);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: ");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView = findViewById(R.id.textView);
+        savedText = savedInstanceState.getString(SAVED_KEY);
+        textView.append("\n" + "onRestoreInstanceState");
+    }
 }
